8659841230303abfc3b15cc05d24486a1241dd12
Riddarvid <44440210+Riddarvid@users.noreply.github.com>
2023-03-05 18:55:41 +0100

    Infrastructure, solve Day1

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,24 @@
 module Main (main) where
 
-import Lib
+import Solution (Solution)
+import Day1 (solve)
+
+day :: Int
+day = 1
 
 main :: IO ()
-main = someFunc
+main = do
+  input <- readFile ("input/input" ++ show day ++ ".txt")
+  let solution = (solvers !! (day - 1)) input
+  printSolution solution
+
+solvers :: [String -> Solution]
+solvers = [Day1.solve]
+
+printSolution :: Solution -> IO ()
+printSolution (part1, part2) = do
+  putStrLn "Part1:"
+  putStrLn part1
+  putStrLn ""
+  putStrLn "Part2:"
+  putStrLn part2
---
+++ src/days/Day1.hs
@@ -0,0 +1,34 @@
+module Day1 (solve) where
+
+import Solution (Solution)
+import Data.Set (Set)
+import qualified Data.Set as Set
+
+solve :: String -> Solution
+solve input = (show part1, show part2)
+  where
+    frequencies = parse input
+    part1 = solve1 frequencies
+    part2 = solve2 frequencies
+
+parse :: String -> [Int]
+parse = map parseLine . lines
+
+parseLine :: String -> Int
+parseLine ('+' : str) = read str
+parseLine ('-' : str) = - read str
+parseLine _ = error "Invalid input"
+
+solve1 :: [Int] -> Int
+solve1 = sum
+
+solve2 :: [Int] -> Int
+solve2 frequencies = firstTwice (cycle frequencies) Set.empty 0
+
+firstTwice :: [Int] -> Set Int -> Int -> Int
+firstTwice (next : freqs) encountered current 
+  | Set.member current encountered = current
+  | otherwise = firstTwice freqs (Set.insert current encountered) current'
+  where
+    current' = next + current
+firstTwice _ _ _ = error "List should be infinite"
