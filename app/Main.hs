5d2ff5925e096deeafd55813e9ec363a48dbe6c4
Riddarvid <44440210+Riddarvid@users.noreply.github.com>
2023-03-07 16:21:17 +0100

    Solve Day5

--- app/Main.hs
+++ app/Main.hs
@@ -5,9 +5,10 @@ import Day1 (solve)
 import Day2 (solve)
 import Day3 (solve)
 import Day4 (solve)
+import Day5 (solve)
 
 day :: Int
-day = 4
+day = 5
 
 main :: IO ()
 main = do
@@ -16,7 +17,7 @@ main = do
   printSolution solution
 
 solvers :: [String -> Solution]
-solvers = [Day1.solve, Day2.solve, Day3.solve, Day4.solve]
+solvers = [Day1.solve, Day2.solve, Day3.solve, Day4.solve, Day5.solve]
 
 printSolution :: Solution -> IO ()
 printSolution (part1, part2) = do
---
+++ src/days/Day5.hs
@@ -0,0 +1,43 @@
+module Day5 (solve) where
+import Solution (Solver)
+import Data.Char (toLower, isLower, isUpper)
+
+solve :: Solver
+solve input = (show part1, show part2)
+  where
+    polymer = head $ lines input
+    part1 = length $ reduceFully polymer
+    part2 = solve2 polymer
+
+-- General / Part 1
+
+reduceFully :: String -> String
+reduceFully polymer
+  | length polymer == length polymer' = polymer
+  | otherwise = reduceFully polymer'
+  where
+    polymer' = reduce polymer
+
+reduce :: String -> String
+reduce [] = []
+reduce [c] = [c]
+reduce (c1 : c2 : polymer)
+  | canReact c1 c2 = reduce polymer
+  | otherwise = c1 : reduce (c2 : polymer)
+
+canReact :: Char -> Char -> Bool
+canReact c1 c2 = sameUnit c1 c2 && oppositePolarity
+  where
+    oppositePolarity = (isLower c1 && isUpper c2) || (isUpper c1 && isLower c2)
+
+sameUnit :: Char -> Char -> Bool
+sameUnit c1 c2 = toLower c1 == toLower c2
+
+-- Part 2
+
+solve2 :: String -> Int
+solve2 polymer = minimum $ map (length . reduceFully . removeUnit polymer) ['a' .. 'z']
+
+removeUnit :: String -> Char -> String
+removeUnit polymer unit = 
+  foldr (\c polymer' -> if sameUnit c unit then polymer' else c : polymer') "" polymer
