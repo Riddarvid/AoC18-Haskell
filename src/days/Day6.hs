c11dbabef3590440e3b59ea5f20422c6877db623
Riddarvid <44440210+Riddarvid@users.noreply.github.com>
2023-03-08 09:10:58 +0100

    Refactor using sets

--- src/days/Day6.hs
+++ src/days/Day6.hs
@@ -13,7 +13,7 @@ type Pos = (Int, Int)
 solve :: Solver
 solve input = (show part1, "")
   where
-    coords = map parseCoord $ lines input
+    coords = Set.fromList $ map parseCoord $ lines input
     part1 = solve1 coords
 
 -- Parsing
@@ -25,36 +25,38 @@ parseCoord str = (tokens !! 0, tokens !! 1)
 
 -- Part 1
 
-solve1 :: [Pos] -> Int
-solve1 coords = maximum $ filter (< limit) $ map (closestArea coords limit) coords
+solve1 :: Set Pos -> Int
+solve1 coords = maximum $ Set.filter (< limit) $ Set.map (closestArea coords limit) coords
   where
     limit = findLimit coords
 
-closestArea :: [Pos] -> Int -> Pos -> Int
+closestArea :: Set Pos -> Int -> Pos -> Int
 closestArea coords limit current = Set.size $ reachableBFSLimit current limit adjacency options
   where
     options = BFSOptions {keepVisited = True, pruneFun = Nothing}
     adjacency = areaAdjecency coords current
 
-findLimit :: [Pos] -> Int
+findLimit :: Set Pos -> Int
 findLimit _ = 5000 -- TODO fix
 
-areaAdjecency :: [Pos] -> Pos -> Pos -> [Pos]
+areaAdjecency :: Set Pos -> Pos -> Pos -> [Pos]
 areaAdjecency coords current pos = filter (isCloserTo current coords) neighbors
   where
     neighbors = getNeighbors pos
 
 getNeighbors :: Pos -> [Pos]
-getNeighbors (x, y) = 
+getNeighbors (x, y) =
   [
-    (x + 1, y), 
-    (x - 1, y), 
-    (x, y + 1), 
+    (x + 1, y),
+    (x - 1, y),
+    (x, y + 1),
     (x, y - 1)]
 
-isCloserTo :: Pos -> [Pos] -> Pos -> Bool
-isCloserTo target coords pos = 
-  all (\coord -> (coord == target) || (distance pos target < distance pos coord)) coords
+isCloserTo :: Pos -> Set Pos -> Pos -> Bool
+isCloserTo target coords pos =
+  all (\coord -> distance pos target < distance pos coord) coords'
+  where
+    coords' = Set.delete target coords
 
 distance :: Pos -> Pos -> Int
 distance (x1, y1) (x2, y2) = abs (x1 - x2) + abs (y1 - y2)
